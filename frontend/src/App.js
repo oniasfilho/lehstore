f6034eae2d0584cf89061400bd6f83c9d615912c
Onias da Rocha <onias.filho@gmail.com>
2022-04-23 23:57:01 -0400

    finishes first part of the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './components/screens/HomeScreen'
+import ProductScreen from './components/screens/ProductScreen'
+
 function App() {
-  return <>Bem vindo!</>
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
