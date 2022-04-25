7f5270ca0ccd0b2deb848934d06330ab68ca8e43
Onias da Rocha <onias.filho@gmail.com>
2022-04-25 12:06:13 -0400

    started the backend, removed products.json from the frontend folder

--- frontend/src/components/screens/ProductScreen.jsx
+++ frontend/src/components/screens/ProductScreen.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../Rating'
-import products from '../../products'
+import axios from 'axios'
 
 const ProductScreen = () => {
   const { id } = useParams()
-  const product = products.find((p) => p._id === id)
+
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+
+      console.log(id)
+    }
+
+    fetchProduct()
+  }, [])
 
   return (
     <>
