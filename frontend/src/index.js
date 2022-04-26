6f2ef887d401a77f7694525154fada8ea76d0c3b
Onias da Rocha <onias.filho@gmail.com>
2022-04-26 17:12:38 -0400

    starts fetching data from the backend into the redux storage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store.js' // inserir .js
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
+  // <Provider store={store}>
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
