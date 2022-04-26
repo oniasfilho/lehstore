e35e5e186476bb755a683326f9fdf35afeab0509
Onias da Rocha <onias.filho@gmail.com>
2022-04-26 17:44:58 -0400

    creates loader and message component, also implements redux usage in the products detail screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,10 +6,14 @@ import {
 } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers.js'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers.js'
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 const initialState = {}
 const middleware = [thunk]
