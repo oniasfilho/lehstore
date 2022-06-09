3acd728763d5751abc4574b843ece16c06489ede
Onias da Rocha <onias.filho@gmail.com>
2022-06-09 01:10:09 -0400

    implements user creation functionality via frontend | s8v47

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './components/screens/HomeScreen'
 import ProductScreen from './components/screens/ProductScreen'
 import CartScreen from './components/screens/CartScreen'
 import LoginScreen from './components/screens/LoginScreen'
+import RegisterScreen from './components/screens/RegisterScreen'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/cart/" element={<CartScreen />} />
